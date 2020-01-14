6fbcd145365b908103f06afe1c484c24e8cff2df
ashish001rathod <59167059+ashish001rathod@users.noreply.github.com>
2020-01-14 17:44:15 +0530

    Add files via upload

---
+++ emptask/src/com/deloitte/empl/applications/GetEmpByCode.java
@@ -0,0 +1,17 @@
+package com.deloitte.empl.applications;
+
+import com.deloitte.empl.beans.Emp;
+import com.deloitte.empl.dao.EmpDao;
+import com.deloitte.empl.dao.impl.EmpDaoImpl;
+
+public class GetEmpByCode {
+	public static void main(String[] args) {
+		EmpDao dao = new EmpDaoImpl();
+		Emp e = dao.getEmpByCode(7782);
+		System.out.println(e.getEmpno()+" "+e.getEname()+" "+e.getJob()+" "+
+				e.getMgr()+" "+e.getHiredate()+" "+e.getSal()+" "+e.getComm()+" "+
+				e.getDeptno());
+	}
+
+
+}
